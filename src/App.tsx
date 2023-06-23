4c2390e966ae48ba33d3a3c09cae4c21b9124b80
AdorraBell <63513059+AdorraBell@users.noreply.github.com>
2023-06-23 20:34:35 +0600

    ready base project configuration, theme switching functionality

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { Suspense } from "react";
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import "./styles/index.scss";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
+
+
+
+const App = () => {
+
+    const {theme, toggleTheme} = useTheme();
+
+    return ( 
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>
+                Change Theme
+            </button>
+            <div>
+                <Link to="/about">About</Link>
+                <Link to="/">Home</Link>
+            </div>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync />}/>
+                    <Route path={'/'} element={<MainPageAsync />}/>
+                </Routes>
+            </Suspense>
+        </div>
+    );
+}
+ 
+export default App;
