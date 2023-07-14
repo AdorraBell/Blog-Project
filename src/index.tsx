c7cf65b59d1229e23c44cc42327ca8af0391dd4e
AdorraBell <63513059+AdorraBell@users.noreply.github.com>
2023-07-14 19:23:33 +0600

    bundle analyzer added to the project, changed max length for string (eslint settings)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import { render } from 'react-dom';
-import React from 'react';
 import App from 'app/App';
 import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from 'app/providers/ThemeProvider/ui/ThemeProvider';
