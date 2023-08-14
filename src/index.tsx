46db10bf51bfc17a2c331064fbc40835b39ec8c0
Aletta <63513059+AdorraBell@users.noreply.github.com>
2023-08-15 03:45:45 +0600

    Revert "added redux-toolkit and first entity"

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import ThemeProvider from 'app/providers/ThemeProvider/ui/ThemeProvider';
 import 'shared/config/i18n/i18n';
 import { ErrorBoundary } from 'app/providers/ErrorBoundary';
-import { StoreProvider } from 'app/providers/StoreProvider';
 import 'app/styles/index.scss';
 
 render(
-  <StoreProvider>
-    <BrowserRouter>
-      <ErrorBoundary>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </ErrorBoundary>
-    </BrowserRouter>
-  </StoreProvider>,
-
+  <BrowserRouter>
+    <ErrorBoundary>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
